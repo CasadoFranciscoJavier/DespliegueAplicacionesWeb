4d503ae1eff228e52dccdc5b5bb4a4d8d2ecf88c
Javica <franciscojaviercasado001.alu@stemgranada.com>
2025-11-17 12:15:13 +0100

    feat: wip docker tienda animales

---
+++ UD2/tienda-animales-docker/mysql/init.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE tienda_animales;
+
+USE tienda_animales;
+
+CREATE TABLE mascotas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    tipo VARCHAR(30) NOT NULL,
+    edad INT NOT NULL,
+    disponible BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+INSERT INTO mascotas (nombre, tipo, edad, disponible) VALUES
+('Luna', 'Perro', 2, TRUE),
+('Michi', 'Gato', 3, TRUE),
+('Rocky', 'Perro', 1, TRUE),
+('Nube', 'Conejo', 1, FALSE),
+('Piolín', 'Ave', 4, TRUE);
